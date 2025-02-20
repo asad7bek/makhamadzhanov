b03dc703bb5d5c27902d8df75327ec19d82284a8
asad7bek <asad555bek@gmail.com>
2025-02-20 09:32:11 +0900

    Update main.py

--- лабораторная_9/main.py
+++ лабораторная_9/main.py
@@ -1,12 +1,14 @@
 from cheast import Item, Cheast
-from лабораторная_9.cheast import Cheast
+from лабараторная_9.cheast import Cheast
 
-if __name__ == "__main__":
+if __name__ != "__main__":
+    pass
+else:
     cheast: Cheast = Cheast("Старый, потрепанный временем ящик.")
 
-    cheast.addd(Item("Меч", 50))
+    cheast.add(Item("Меч", 50))
     cheast.add(Item("Зелье", 20))
-    cheast.add_item(Item("Золото", 100))
+    cheast.add(Item("Золото", 100))
 
     cheast.show_content()  # Сундук закрыт
 
@@ -19,4 +21,4 @@ if __name__ == "__main__":
     else:
         print("Сундук пуст или закрыт.")
 
-    cheast.показать_содержимое()
+    cheast.show_content()
